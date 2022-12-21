12b0bdede8a500780e480cedf98303e1c9e4ddbf
changheonkim <heon5652@gmail.com>
2022-12-21 22:09:16 +0900

    start

--- app/src/main/java/org/changheon/test/MainActivity.java
+++ app/src/main/java/org/changheon/test/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// androidx는 새로운 버전이다. 전 시리즈와의 호환을 위해서는 예전 버전의 라이브러리로 자동변환을 해줘야한다.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
